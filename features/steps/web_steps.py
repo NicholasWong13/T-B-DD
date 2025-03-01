d12e1ed6407eb25b83d7296f6702e7d8280f663a
NicholasWong13 <103627168+NicholasWong13@users.noreply.github.com>
2025-03-01 15:17:35 +0800

    Update web_steps.py

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -104,8 +104,49 @@ def step_impl(context, element_name):
 # to get the element id of any button
 ##################################################################
 
-## UPDATE CODE HERE ##
+@when('I press the "{button}" button')
+def step_impl(context, button):
+    button_id = button.lower() + '-btn'
+    context.driver.find_element_by_id(button_id).click()
 
+
+@then('I should see "{name}" in the results')
+def step_impl(context, name):
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'search_results'),
+            name
+        )
+    )
+    assert(found)
+
+@then('I should see the message "{message}"')
+def step_impl(context, message):
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'flash_message'),
+            message
+        )
+    )
+    assert(found)
+
+@then(u'I should not see "Shoes" in the results')
+def step_impl(context):
+    element = context.driver.find_element(By.ID, 'search_results')
+    assert "Shoes" not in element.text
+
+
+@then(u'I should not see "Hat" in the results')
+def step_impl(context):
+    element = context.driver.find_element(By.ID, 'search_results')
+    assert "Hat" not in element.text
+
+
+@then(u'I should not see "Sheets" in the results')
+def step_impl(context):
+    element = context.driver.find_element(By.ID, 'search_results')
+    assert "Sheets" not in element.text
+    
 ##################################################################
 # This code works because of the following naming convention:
 # The id field for text input in the html is the element name
